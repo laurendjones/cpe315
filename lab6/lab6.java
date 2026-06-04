3cc194d97e0a9cb5112683b6f841a40e1da3a012
Sean <sean101tracy@gmail.com>
2026-06-03 21:57:52 -0700

    starting Cache class

--- lab6/lab6.java
+++ lab6/lab6.java
@@ -1,5 +1,34 @@
 package lab6;
+import java.io.*;
+import java.util.*;
 
 public class lab6 {
     
+}
+
+class Cache {
+    final int numSets;
+    final int ways;
+    final int offset;
+    final int indexBits;
+    final int indexMask;
+    final int[][] tags;
+    final int[][] lruCounter;
+ 
+    long hits = 0;
+    long accesses = 0;
+
+    Cache(int cacheSize, int ways, int blockSize) {
+        this.ways = ways;
+        this.offset = Integer.numberOfTrailingZeros(blockSize);
+        this.numSets = cacheSize / (blockSize * ways);
+        this.indexBits = Integer.numberOfTrailingZeros(numSets);
+        this.indexMask = numSets - 1;
+        this.tags = new int[numSets][ways];
+        this.lruCounter = new int[numSets][ways];
+
+        for (int[] row : tags) {
+            Arrays.fill(row, -1);
+        }
+    }
 }
